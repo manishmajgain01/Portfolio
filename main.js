67f0a6135ffc5f1cca357ee828294406539ff073
Manish Majgain <manishmajgain@Manishs-MacBook-Air.local>
2026-06-14 22:18:48 +0545

    animation removed

--- main.js
+++ main.js
@@ -19,66 +19,67 @@ const scrollRevealOption = {
   origin: "bottom",
   duration: 400,
 };
+// this is also commented out
 
-ScrollReveal().reveal(".header__image img", {
-  ...scrollRevealOption,
-  origin: "right"
-});
-ScrollReveal().reveal(".header__content h2", {
-  ...scrollRevealOption,
-  delay: 100,
-});
-ScrollReveal().reveal(".header__content h1", {
-  ...scrollRevealOption,
-  delay: 200,
-});
-ScrollReveal().reveal(".header__content h3", {
-  ...scrollRevealOption,
-  delay: 300,
-});
-ScrollReveal().reveal(".header__content p", {
-  ...scrollRevealOption,
-  delay: 400,
-});
-ScrollReveal().reveal(".header__btns", {
-  ...scrollRevealOption,
-  delay: 500,
-});
+// ScrollReveal().reveal(".header__image img", {
+//   ...scrollRevealOption,
+//   origin: "right"
+// });
+// ScrollReveal().reveal(".header__content h2", {
+//   ...scrollRevealOption,
+//   delay: 100,
+// });
+// ScrollReveal().reveal(".header__content h1", {
+//   ...scrollRevealOption,
+//   delay: 200,
+// });
+// ScrollReveal().reveal(".header__content h3", {
+//   ...scrollRevealOption,
+//   delay: 300,
+// });
+// ScrollReveal().reveal(".header__content p", {
+//   ...scrollRevealOption,
+//   delay: 400,
+// });
+// ScrollReveal().reveal(".header__btns", {
+//   ...scrollRevealOption,
+//   delay: 500,
+// });
 
-ScrollReveal().reveal(".intro__image", {
-  ...scrollRevealOption,
-  origin: "left",
-});
-ScrollReveal().reveal(".intro__content .section__subheader", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-ScrollReveal().reveal(".intro__content .section__header", {
-  ...scrollRevealOption,
-  delay: 100,
-});
-ScrollReveal().reveal(".intro__description", {
-  ...scrollRevealOption,
-  delay: 200,
-});
-ScrollReveal().reveal(".intro__grid", {
-  ...scrollRevealOption,
-  delay: 300,
-});
-ScrollReveal().reveal(".intro__content h4", {
-  ...scrollRevealOption,
-  delay: 400,
-});
-ScrollReveal().reveal(".intro__flex div", {
-  ...scrollRevealOption,
-  delay: 500,
-  interval: 100,
-});
+// ScrollReveal().reveal(".intro__image", {
+//   ...scrollRevealOption,
+//   origin: "left",
+// });
+// ScrollReveal().reveal(".intro__content .section__subheader", {
+//   ...scrollRevealOption,
+//   delay: 500,
+// });
+// ScrollReveal().reveal(".intro__content .section__header", {
+//   ...scrollRevealOption,
+//   delay: 100,
+// });
+// ScrollReveal().reveal(".intro__description", {
+//   ...scrollRevealOption,
+//   delay: 200,
+// });
+// ScrollReveal().reveal(".intro__grid", {
+//   ...scrollRevealOption,
+//   delay: 300,
+// });
+// ScrollReveal().reveal(".intro__content h4", {
+//   ...scrollRevealOption,
+//   delay: 400,
+// });
+// ScrollReveal().reveal(".intro__flex div", {
+//   ...scrollRevealOption,
+//   delay: 500,
+//   interval: 100,
+// });
 
-ScrollReveal().reveal(".journey__grid > div > div", {
-  ...scrollRevealOption,
-  interval: 100,
-});
+// ScrollReveal().reveal(".journey__grid > div > div", {
+//   ...scrollRevealOption,
+//   interval: 100,
+// });
 
 const filterButtons = document.querySelectorAll(".filter-btn");
 const projectCards = document.querySelectorAll(".project-card");
@@ -108,18 +109,19 @@ setActiveFilter("all");
 //   distance: "30px",
 //   origin: "bottom",
 // });
+//this is also commented out
 
-ScrollReveal().reveal(".banner__content .section__header", {
-  ...scrollRevealOption,
-});
-ScrollReveal().reveal(".banner__content p", {
-  ...scrollRevealOption,
-  delay: 100,
-});
-ScrollReveal().reveal(".banner__btn", {
-  ...scrollRevealOption,
-  delay: 200,
-});
+// ScrollReveal().reveal(".banner__content .section__header", {
+//   ...scrollRevealOption,
+// });
+// ScrollReveal().reveal(".banner__content p", {
+//   ...scrollRevealOption,
+//   delay: 100,
+// });
+// ScrollReveal().reveal(".banner__btn", {
+//   ...scrollRevealOption,
+//   delay: 200,
+// });
 
 const swiper = new Swiper(".swiper", {
   slidesPerView: 2,
@@ -130,8 +132,8 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
   },
 });
-
-ScrollReveal().reveal(".blog__card", {
-  ...scrollRevealOption,
-  interval: 500,
-});
+// this is also commented out
+// ScrollReveal().reveal(".blog__card", {
+//   ...scrollRevealOption,
+//   interval: 500,
+// });
